ee8ce1366a5d8521ca6c3f86dd864d562613a3e3
Tahsin Ahmed Tanvir <tahsinahmed.std@gmail.com>
2025-12-29 14:56:03 +0600

    create a middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json('User created successfully!');
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
 
