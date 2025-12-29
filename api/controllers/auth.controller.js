60bbde755e5e9fd16f44830f70bd94311441e0cc
Tahsin Ahmed Tanvir <tahsinahmed.std@gmail.com>
2025-12-30 05:01:51 +0600

    update header,api,profile,user routes,user controller, auth route and create listing

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -33,7 +33,6 @@ export const signin = async (req, res, next) => {
   }
 };
 
-
 export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
@@ -65,6 +64,15 @@ export const google = async (req, res, next) => {
         .status(200)
         .json(rest);
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('User has been logged out!');
   } catch (error) {
     next(error);
   }
