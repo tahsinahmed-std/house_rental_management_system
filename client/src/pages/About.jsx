ce31e233be34a08097d24f3e753fd457b9556e1f
Tahsin Ahmed Tanvir <tahsinahmed.std@gmail.com>
2025-12-30 12:18:13 +0600

    Complete About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+   <div className='py-20 px-4 max-w-6xl mx-auto'>
+  <h1 className='text-3xl font-bold mb-4 text-slate-800'>About ToLet360</h1>
+
+  <p className='mb-4 text-slate-700'>
+    ToLet360 is a web-based house rental management system developed as an academic project. The purpose of this platform is to simplify the process of renting houses by creating a digital connection between property owners and tenants.
+  </p>
+
+  <p className='mb-4 text-slate-700'>
+    This system allows users to search for available rental houses, view property details, and communicate with house owners easily. Property owners can list their houses, manage rental information, and handle tenant requests through a simple and user-friendly interface.
+  </p>
+
+  <p className='mb-4 text-slate-700'>
+    The goal of ToLet360 is to reduce the complexity of traditional house renting systems and provide a reliable, efficient, and transparent solution using modern web technologies. This project demonstrates practical implementation of real-world problem solving through software development.
+  </p>
+</div>
   )
 }
